61dca74bb472951aea999aa4df5fa0b4f71e2927
dtheobald58 <dtheobald58@yahoo.com>
2016-09-08 22:41:49 -0400

    first commit

---
+++ OddNumbersTheobald/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddNumbersTheobald
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter;
+            for(counter = 1; counter <= 99; counter += 2)
+            {
+                Console.WriteLine(counter);
+            }
+        }
+    }
+}
